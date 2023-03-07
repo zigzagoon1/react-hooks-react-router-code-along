aa52580616e97db333c18adbe795f847bb3b5dff
KellyMark <kmark602@gmail.com>
2023-03-07 15:31:05 -0500

    commit tree 0f9e6fa1065d6a6ed0fe5ad4976906004081a936

--- src/index.js
+++ src/index.js
@@ -1,5 +1,27 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import { BrowserRouter, Route, Switch, NavLink } from "react-router-dom";
+
+
+const linkStyles = {
+  display: "inline-block",
+  width: "50px",
+  padding: "12px",
+  margin: "0 6px 6px",
+  background: "blue",
+  textDecoration: "none",
+  color: "white",
+};
+
+function NavBar() {
+  return (
+    <div>
+      <NavLink to="/" exact style={linkStyles} activeStyle={{background: "darkblue"}}>Home</NavLink>
+      <NavLink to="/about" exact style={linkStyles} activeStyle={{background: "darkblue"}}>About</NavLink>
+      <NavLink to="/login" exact style={linkStyles} activeStyle={{background: "darkblue"}}>Login</NavLink>
+    </div>
+  )
+}
 
 function Home() {
   return (
@@ -9,8 +31,48 @@ function Home() {
   );
 }
 
-function App() {
-  return <Home />;
+function About() {
+  return (
+    <div>
+      <h1>This is my about component!</h1>
+    </div>
+  );
 }
 
-ReactDOM.render(<App />, document.getElementById("root"));
+function Login() {
+  return (
+    <div>
+      <h1>Login</h1>
+      <form>
+        <div>
+          <input type="text" name="username" placeholder="Username" />
+        </div>
+        <div>
+          <input type="password" name="password" placeholder="Password" />
+        </div>
+        <input type="submit" value="Submit" />
+      </form>
+    </div>
+  );
+}
+
+function App() {
+  return (
+    <div>
+    <NavBar />
+    <Switch> 
+        <Route exact path="/">
+          <Home />
+        </Route>
+        <Route path="/about">
+          <About />
+        </Route>
+        <Route path="/login">
+          <Login />
+        </Route>
+      </Switch>
+    </div>
+  )
+}
+
+ReactDOM.render(<BrowserRouter><App /></BrowserRouter>, document.getElementById("root"));
